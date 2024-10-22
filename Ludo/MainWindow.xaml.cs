6f6863cdfd46421fe22f235f76cd9f0c261b5c62
eshog <eshog@BOOK-USM9MV6IU2>
2024-10-22 22:38:28 +0200

    Ludo

--- Ludo/MainWindow.xaml.cs
+++ Ludo/MainWindow.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace LudoGame
 {
@@ -14,57 +11,59 @@ namespace LudoGame
         private int _diceValue;
         private int _currentPlayer = 1; // 1: Red, 2: Yellow, 3: Blue, 4: Green
 
-        // Path coordinates for all players based on a standard Ludo board
-        private Point[] redPath =
+        // Define a class for player data to reduce redundancy
+        private class Player
         {
-            new Point(100, 200), new Point(120, 200), new Point(140, 200), new Point(160, 200),
-            new Point(180, 200), new Point(200, 180), new Point(200, 160), new Point(200, 140),
-            new Point(200, 120), new Point(200, 100), // Red's path continues...
-        };
+            public Point[] Path { get; set; }
+            public Ellipse Piece { get; set; }
+            public int[] PiecePositions { get; set; }
+            public int GoalCount { get; set; } = 0;
+        }
 
-        private Point[] yellowPath =
-        {
-            new Point(300, 100), new Point(300, 120), new Point(300, 140), new Point(300, 160),
-            new Point(300, 180), new Point(280, 200), new Point(260, 200), new Point(240, 200),
-            new Point(220, 200), new Point(200, 200), // Yellow's path continues...
-        };
+        private Player redPlayer, yellowPlayer, bluePlayer, greenPlayer;
 
-        private Point[] bluePath =
-        {
-            new Point(100, 300), new Point(120, 300), new Point(140, 300), new Point(160, 300),
-            new Point(180, 300), new Point(200, 320), new Point(200, 340), new Point(200, 360),
-            new Point(200, 380), new Point(200, 400), // Blue's path continues...
-        };
-
-        private Point[] greenPath =
-        {
-            new Point(300, 300), new Point(300, 320), new Point(300, 340), new Point(300, 360),
-            new Point(300, 380), new Point(280, 400), new Point(260, 400), new Point(240, 400),
-            new Point(220, 400), new Point(200, 400), // Green's path continues...
-        };
-
-        // Track positions of each player's pieces on their respective paths
-        private int[] redPiecePositions = { -1, -1, -1, -1 }; // -1 means in base
-        private int[] yellowPiecePositions = { -1, -1, -1, -1 };
-        private int[] bluePiecePositions = { -1, -1, -1, -1 };
-        private int[] greenPiecePositions = { -1, -1, -1, -1 };
-
-        // Number of pieces that have reached the goal
-        private int redGoalCount = 0;
-        private int yellowGoalCount = 0;
-        private int blueGoalCount = 0;
-        private int greenGoalCount = 0;
-
-        public Ellipse YellowPiece1 { get; private set; }
         public Ellipse GreenPiece1 { get; private set; }
         public Ellipse BluePiece1 { get; private set; }
+        public Ellipse YellowPiece1 { get; private set; }
         public Ellipse RedPiece1 { get; private set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializePlayers();
             UpdateCurrentPlayerText();
             UpdateDiceGraphic(1); // Initialize the dice graphic to show "1"
+        }
+
+        private void InitializePlayers()
+        {
+            redPlayer = new Player
+            {
+                Path = new Point[] { new Point(100, 200), new Point(120, 200), new Point(140, 200), new Point(160, 200), new Point(180, 200), new Point(200, 180) },
+                Piece = RedPiece1,
+                PiecePositions = new int[] { -1 } // -1 means not yet on board
+            };
+
+            yellowPlayer = new Player
+            {
+                Path = new Point[] { new Point(300, 100), new Point(300, 120), new Point(300, 140), new Point(300, 160), new Point(300, 180), new Point(280, 200) },
+                Piece = YellowPiece1,
+                PiecePositions = new int[] { -1 }
+            };
+
+            bluePlayer = new Player
+            {
+                Path = new Point[] { new Point(100, 300), new Point(120, 300), new Point(140, 300), new Point(160, 300), new Point(180, 300), new Point(200, 320) },
+                Piece = BluePiece1,
+                PiecePositions = new int[] { -1 }
+            };
+
+            greenPlayer = new Player
+            {
+                Path = new Point[] { new Point(300, 300), new Point(300, 320), new Point(300, 340), new Point(300, 360), new Point(300, 380), new Point(280, 400) },
+                Piece = GreenPiece1,
+                PiecePositions = new int[] { -1 }
+            };
         }
 
         // Dice roll event handler
@@ -137,188 +136,72 @@ namespace LudoGame
         // Move player piece based on dice roll
         private void MovePlayerPiece()
         {
-            switch (_currentPlayer)
+            Player currentPlayer = GetCurrentPlayer();
+
+            if (currentPlayer.PiecePositions[0] == -1 && _diceValue == 6)
             {
-                case 1:
-                    MoveRedPiece();
-                    break;
-                case 2:
-                    MoveYellowPiece();
-                    break;
-                case 3:
-                    MoveBluePiece();
-                    break;
-                case 4:
-                    MoveGreenPiece();
-                    break;
+                currentPlayer.PiecePositions[0] = 0;
+                AnimatePieceMovement(currentPlayer.Piece, new Point(30, 30), currentPlayer.Path[0]);
+            }
+            else if (currentPlayer.PiecePositions[0] != -1)
+            {
+                int newPosition = currentPlayer.PiecePositions[0] + _diceValue;
+                if (newPosition < currentPlayer.Path.Length)
+                {
+                    Point startPosition = currentPlayer.Path[currentPlayer.PiecePositions[0]];
+                    Point endPosition = currentPlayer.Path[newPosition];
+                    AnimatePieceMovement(currentPlayer.Piece, startPosition, endPosition);
+                    currentPlayer.PiecePositions[0] = newPosition;
+                }
+                else
+                {
+                    currentPlayer.GoalCount++;
+                }
             }
 
-            CheckForWin();
+            CheckForWin(currentPlayer);
+            NextTurn();
+        }
 
-            // Move to the next player
+        private Player GetCurrentPlayer()
+        {
+            return _currentPlayer switch
+            {
+                1 => redPlayer,
+                2 => yellowPlayer,
+                3 => bluePlayer,
+                4 => greenPlayer,
+                _ => null
+            };
+        }
+
+        private void CheckForWin(Player player)
+        {
+            if (player.GoalCount == 1) // Change this if more pieces
+            {
+                MessageBox.Show($"{GetPlayerColor(player)} Wins!");
+            }
+        }
+
+        private string GetPlayerColor(Player player)
+        {
+            if (player == redPlayer) return "Red";
+            if (player == yellowPlayer) return "Yellow";
+            if (player == bluePlayer) return "Blue";
+            return "Green";
+        }
+
+        private void NextTurn()
+        {
             _currentPlayer = _currentPlayer == 4 ? 1 : _currentPlayer + 1;
             UpdateCurrentPlayerText();
         }
 
-        private void MoveRedPiece()
+        // New event handler for the 'MoveRedPlayer_Click' error
+        private void MoveRedPlayer_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 4; i++)
-            {
-                if (redPiecePositions[i] == -1 && _diceValue == 6)
-                {
-                    redPiecePositions[i] = 0;
-                    AnimatePieceMovement(RedPiece1, new Point(50, 50), redPath[0]);
-                    return;
-                }
-                else if (redPiecePositions[i] != -1)
-                {
-                    int newPosition = redPiecePositions[i] + _diceValue;
-                    if (newPosition < redPath.Length)
-                    {
-                        Point startPosition = redPath[redPiecePositions[i]];
-                        Point endPosition = redPath[newPosition];
-                        AnimatePieceMovement(RedPiece1, startPosition, endPosition);
-                        redPiecePositions[i] = newPosition;
-
-                        CheckForOpponentCapture(newPosition, redPath, 1);
-                    }
-                    else
-                    {
-                        redGoalCount++;
-                    }
-                    return;
-                }
-            }
-        }
-
-        private void MoveYellowPiece()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (yellowPiecePositions[i] == -1 && _diceValue == 6)
-                {
-                    yellowPiecePositions[i] = 0;
-                    AnimatePieceMovement(YellowPiece1, new Point(350, 50), yellowPath[0]);
-                    return;
-                }
-                else if (yellowPiecePositions[i] != -1)
-                {
-                    int newPosition = yellowPiecePositions[i] + _diceValue;
-                    if (newPosition < yellowPath.Length)
-                    {
-                        Point startPosition = yellowPath[yellowPiecePositions[i]];
-                        Point endPosition = yellowPath[newPosition];
-                        AnimatePieceMovement(YellowPiece1, startPosition, endPosition);
-                        yellowPiecePositions[i] = newPosition;
-
-                        CheckForOpponentCapture(newPosition, yellowPath, 2);
-                    }
-                    else
-                    {
-                        yellowGoalCount++;
-                    }
-                    return;
-                }
-            }
-        }
-
-        private void MoveBluePiece()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (bluePiecePositions[i] == -1 && _diceValue == 6)
-                {
-                    bluePiecePositions[i] = 0;
-                    AnimatePieceMovement(BluePiece1, new Point(50, 350), bluePath[0]);
-                    return;
-                }
-                else if (bluePiecePositions[i] != -1)
-                {
-                    int newPosition = bluePiecePositions[i] + _diceValue;
-                    if (newPosition < bluePath.Length)
-                    {
-                        Point startPosition = bluePath[bluePiecePositions[i]];
-                        Point endPosition = bluePath[newPosition];
-                        AnimatePieceMovement(BluePiece1, startPosition, endPosition);
-                        bluePiecePositions[i] = newPosition;
-
-                        CheckForOpponentCapture(newPosition, bluePath, 3);
-                    }
-                    else
-                    {
-                        blueGoalCount++;
-                    }
-                    return;
-                }
-            }
-        }
-
-        private void MoveGreenPiece()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (greenPiecePositions[i] == -1 && _diceValue == 6)
-                {
-                    greenPiecePositions[i] = 0;
-                    AnimatePieceMovement(GreenPiece1, new Point(350, 350), greenPath[0]);
-                    return;
-                }
-                else if (greenPiecePositions[i] != -1)
-                {
-                    int newPosition = greenPiecePositions[i] + _diceValue;
-                    if (newPosition < greenPath.Length)
-                    {
-                        Point startPosition = greenPath[greenPiecePositions[i]];
-                        Point endPosition = greenPath[newPosition];
-                        AnimatePieceMovement(GreenPiece1, startPosition, endPosition);
-                        greenPiecePositions[i] = newPosition;
-
-                        CheckForOpponentCapture(newPosition, greenPath, 4);
-                    }
-                    else
-                    {
-                        greenGoalCount++;
-                    }
-                    return;
-                }
-            }
-        }
-
-        private void CheckForOpponentCapture(int position, Point[] playerPath, int currentPlayer)
-        {
-            if (currentPlayer == 1)
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    if (yellowPiecePositions[i] != -1 && playerPath[position] == yellowPath[yellowPiecePositions[i]])
-                    {
-                        yellowPiecePositions[i] = -1; // Send yellow piece back to base
-                        AnimatePieceMovement(YellowPiece1, yellowPath[yellowPiecePositions[i]], new Point(350, 50));
-                    }
-                    // Add logic for blue and green pieces
-                }
-            }
-            // Add similar checks for other players
-        }
-
-        private void CheckForWin()
-        {
-            if (redGoalCount == 4)
-            {
-                MessageBox.Show("Red Wins!");
-            }
-            else if (yellowGoalCount == 4)
-            {
-                MessageBox.Show("Yellow Wins!");
-            }
-            else if (blueGoalCount == 4)
-            {
-                MessageBox.Show("Blue Wins!");
-            }
-            else if (greenGoalCount == 4)
-            {
-                MessageBox.Show("Green Wins!");
-            }
+            // This is just a sample action. You can either trigger movement or dice roll here
+            MovePlayerPiece(); // Move red piece (or you can put specific logic for red here)
         }
     }
 }
